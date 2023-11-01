c695bb73fbef09e6402ee3e29eee475a55b14c63
Santhush98 <149574378+Santhush98@users.noreply.github.com>
2023-11-01 14:52:06 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,1 +1,13 @@
+# Function to calculate the area of a circle
+def calculate_circle_area(radius):
+    return 3.14159 * radius**2
+
+# Taking user input for the radius of the circle
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculating the area of the circle
+area = calculate_circle_area(radius)
+
+# Displaying the calculated area
+print(f"The area of a circle with radius {radius} is: {area}")
 
